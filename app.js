434bd07a4b21d1dfcecbb3260eaf0f14cd1a8e37
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2024-12-13 19:24:48 +0530

    added route for finding a specific tour by id

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.json());
 const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8');
 const toursObj = JSON.parse(tours);
 
+
+// GET api/v1/tours
 app.get('/api/v1/tours', (request, response) => {
   response.status(200).json({
     message: "Data is available 👍",
@@ -22,6 +24,32 @@ app.get('/api/v1/tours', (request, response) => {
 })
 
 
+// GET api/v1/tours/:id
+app.get('/api/v1/tours/:id', (request, response) => {
+  const id = request.params.id * 1 // converting string to number
+  const tour = toursObj.find(el => el.id === id);
+  if (!tour) {
+    response.status(404).send({
+      status: "failure",
+      message: "Tour not found!"
+    })
+  }
+  else {
+    response.status(200).send({
+      status: "success",
+      message: "Tour found!",
+      data: {
+        tour
+      }
+    })
+  }
+  // response.status(200).json({
+  //   message: "Data is available 👍",
+  //   data: toursObj
+  // });
+})
+
+// POST api/v1/tours
 app.post('/api/v1/tours', (request, response) => {
   const newId = toursObj[toursObj.length - 1].id + 1 // create a new id
   const newTour = Object.assign({ id: newId }, request.body);
